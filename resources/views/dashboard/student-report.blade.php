364be091f774c322f4323c45551858889cb2a957
RobiBerhe <berherobi@gmail.com>
2018-10-16 06:00:26 -0700

    Please enter the commit message for your changes. Lines starting

--- resources/views/dashboard/student-report.blade.php
+++ resources/views/dashboard/student-report.blade.php
@@ -95,12 +95,5 @@
 		</div>
 	</div>
 
-
-
-{{-- <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-<script src="{{asset('js/popper.js')}}"></script>
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/materialize.min.js')}}"></script> --}}
-
 </body>
 </html>
